6a951d9096245c57f6744161415b59fb20471f85
Alex Taylor <alexetaylor7@gmail.com>
2024-01-02 14:02:47 -0700

    added comments

--- currency_converter.cpp
+++ currency_converter.cpp
@@ -8,16 +8,21 @@ using namespace std;
 
 int main()
 {
+    // Declare variables
     double dollar;
     string currency;
 
+    // Inform user and prompt
     cout << "CURRENCY CONVERTER\n";
     cout << "Enter USD value you want converted:\n";
     cin >> dollar;
+
+    // Conversion factors for currencies
     double pound = dollar * 0.79;
     double euro = dollar * 0.91;
     double rupee = dollar * 83.27;
 
+    // Prompt user for input and output proper currency
     cout << "Enter GBP for converting USD to GBP, INR for converting USD to INR, or EUR for converting USD to EUR:\n";
     cin >> currency;
 
