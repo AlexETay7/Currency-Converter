1fd9b2f4e6de0de31dada4e42b9bfc40490d14c1
Alex Taylor <alexetaylor7@gmail.com>
2024-01-02 13:29:27 -0700

    added user input functionality

--- currency_converter.cpp
+++ currency_converter.cpp
@@ -16,4 +16,21 @@ int main()
     cin >> dollar;
     double pound = dollar * 0.79;
     double euro = dollar * 0.91;
+    double rupee = dollar * 83.27;
+
+    cout << "Enter GBP for converting USD to GBP, INR for converting USD to INR, or EUR for converting USD to EUR:\n";
+    cin >> currency;
+
+    if (currency == "INR")
+    {
+        cout << dollar << " Dollars in Rupees = " << rupee;
+    }
+    else if (currency == "EUR")
+    {
+        cout << dollar << " Dollars in Euro = " << euro;
+    }
+    else if (currency == "GBP")
+    {
+        cout << dollar << " Dollars in Pound = " << pound;
+    }
 }
